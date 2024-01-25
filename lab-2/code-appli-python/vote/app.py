3f8c058ece678fceb818759bfacbfc6c0ffb3dca
ClementBregeon <142522295+ClementBregeon@users.noreply.github.com>
2024-01-25 16:54:02 +0100

    Update app.py

--- lab-2/code-appli-python/vote/app.py
+++ lab-2/code-appli-python/vote/app.py
@@ -10,7 +10,7 @@ option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
-app = Flask(__name__)
+app = Flask(__name__) 
 
 gunicorn_error_logger = logging.getLogger('gunicorn.error')
 app.logger.handlers.extend(gunicorn_error_logger.handlers)
